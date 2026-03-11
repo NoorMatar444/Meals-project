8c26817e3c8ca7fb64ffd2c2ed69adad2153a7cd
NoorMatar444 <noormatar444@yahoo.com>
2026-03-11 19:46:53 +0200

    update ui

--- app/page.tsx
+++ app/page.tsx
@@ -8,14 +8,12 @@ export default async function Home() {
   console.log(data)
   return (
     <>
-      <div className="container w-[50%] mx-auto ">
-        {data.map((ingredient:ingredientType) => <div key={ingredient.idIngredient} className="cards my-6">
-          <div className="name bg-gray-500 text-white text-center"><p>{ingredient.strIngredient}</p></div>
-          <div className="image flex justify-center"><Image alt='image' src={ingredient.strThumb} width={500} height={500} /></div>
-
-          <div className="description"><p>{ingredient.strDescription}</p></div>
-        </div>)}
-      </div>
+      
+      {data.map((ingredient: ingredientType) => <div key={ingredient.idIngredient} className="cards my-6 container w-[50%] mx-auto shadow-lg rounded-lg p-4 my-6">
+        <div className="name bg-green-500 text-white text-center"><p>{ingredient.strIngredient}</p></div>
+        <div className="image flex justify-center"><Image alt='image' src={ingredient.strThumb} width={500} height={500} /></div>
+        <div className="description"><p>{ingredient.strDescription}</p></div>
+      </div>)}
     </>
   )
 }
